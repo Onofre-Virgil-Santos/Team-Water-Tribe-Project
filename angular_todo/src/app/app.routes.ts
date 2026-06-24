da3d0f19f0ec0d64eb2c60930a7de1ebf578dcd9
Onofre-Virgil-Santos <virgilsantos1999@gmail.com>
2026-06-24 14:40:15 -0400

    Updated with auth and home

--- angular_todo/src/app/app.routes.ts
+++ angular_todo/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Register } from './register/register';
+import { Home } from './home/home';
+import { authGuard } from './auth/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'register', component: Register },
+  { path: 'home', component: Home, canActivate: [authGuard] },
+];
