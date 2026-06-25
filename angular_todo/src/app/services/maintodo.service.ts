732a1d7ca769038b0c09861e5213e30be5a2b4db
Sulakna Karunaratna <sulaknak@gmail.com>
2026-06-25 00:23:35 -0400

    backend and frontend connection

--- angular_todo/src/app/services/maintodo.service.ts
+++ angular_todo/src/app/services/maintodo.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { MainTodo } from '../models/maintodo.model';
 
@@ -12,15 +12,24 @@ export class MainTodoService {
 
   constructor(private http: HttpClient) {}
 
+  private getHeaders(){
+    const token = localStorage.getItem('auth_token');
+    return {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      })
+    };
+  }
+
   getAllTodos(): Observable<MainTodo[]> {
-    return this.http.get<MainTodo[]>(this.apiUrl);
+    return this.http.get<MainTodo[]>(this.apiUrl, this.getHeaders());
   }
 
   createTodo(task: string): Observable<MainTodo> {
-    return this.http.post<MainTodo>(this.apiUrl, { task });
+    return this.http.post<MainTodo>(this.apiUrl, { task }, this.getHeaders());
   }
 
   deleteTodo(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    return this.http.delete<void>(`${this.apiUrl}/${id}`, this.getHeaders());
   }
 }
